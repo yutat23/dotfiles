a029fefd72991902e84570f307ab6a001aeb8f71
yutat23 <yutat23@gmail.com>
2021-01-14 02:24:22 +0900

    tmux

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 #neovim
 ln -sf ~/dotfiles/.config ~/.config
