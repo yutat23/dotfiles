71f7c5ca655d2ab857d6d140df9c17be4b33aea1
yutat23 <yutat23@gmail.com>
2023-03-10 03:13:45 +0900

    fish

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,3 +7,5 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 #neovim
 ln -sf ~/dotfiles/.config ~/.config
+#fish
+ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+#peco setting
+set fish_plugins theme peco
+
+function fish_user_key_bindings
+bind \cw peco_select_history
+end
