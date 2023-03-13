3820f9fb10b364debcf97bae17a3795f4236b43b
yutat23 <yutat23@gmail.com>
2023-03-13 15:00:59 +0900

    rm fish sl

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,5 +7,3 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 #neovim
 ln -sf ~/dotfiles/.config ~/.config
-#fish
-ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
