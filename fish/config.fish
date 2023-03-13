a4053e4bda59f8fc89cb99eeee04bf04019d760d
yutat23 <yutat23@gmail.com>
2023-03-13 14:20:46 +0900

    alias, exa

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,36 @@
+#alias
+alias watch="watch -c "
+
+if type exa >/dev/null 2>&1;
+  alias l='exa --icons --color always'
+  alias ll='exa -la --icons --color always'
+else
+  alias l='ls --color=auto'
+  alias ll='ls --color=auto -la'
+end
+
+if type bat >/dev/null 2>&1;
+  export BAT_THEME="Monokai Extended"
+end
+
+if type fdfind >/dev/null 2>&1;
+  alias fd='fdfind'
+end
+
+
+if type batcat >/dev/null 2>&1;
+  alias bat='batcat'
+end
+
+if type rgrep >/dev/null 2>&1;
+  alias rg='rgrep -n --color'
+end
+
+alias vimr='vim -R'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
 #peco setting
 set fish_plugins theme peco
 
